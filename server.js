3069393f2b7035d7ac1b3aff7ef81f5c361dcd3e
Marcus <marcusmallum@gmail.com>
2018-09-13 21:31:51 -0500

    Database connection added and initial front-end added

--- server.js
+++ server.js
@@ -1,13 +1,51 @@
-let express = require('express');
-let app = express();
+const express = require('express');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+
+const url = 'mongodb://mmallum:abc123@se-test.ddns.net:27017/soccer_roster';
+const dbName = 'soccer_roster';
+
+MongoClient.connect(url, function(err, client) {
+    console.log(err);
+    const db = client.db(dbName);
+    client.close();
+    /*insertDocuments(db, function() {
+        findDocuments(db, function(){
+            client.close();
+        });
+    });*/
+});
+
+const findDocuments = function(db, callback) {
+    // Get the documents collection
+    const collection = db.collection('documents');
+    // Find some documents
+    collection.find({}).toArray(function(err, docs) {
+        console.log("Found the following records");
+        console.log(docs);
+        callback(docs);
+    });
+};
+
+const insertDocuments = function(db, callback) {
+    // Get the documents collection
+    const collection = db.collection('documents');
+    // Insert some documents
+    collection.insertMany([
+        {a : 1}, {a : 2}, {a : 3}
+    ], function(err, result) {
+        console.log("Inserted 3 documents into the collection");
+        callback(result);
+    });
+};
 
 // When client connects, send "Hello World"
 app.get('/', function (req, res) {
-    res.send('Hello World');
+    res.sendFile(__dirname + "/" + "home.html");
 });
 
-// Server listen on port 3000
-let server = app.listen(80, function () {
+// Server listen on port 80
+const server = app.listen(80, function () {
     let port = server.address().port;
 
     console.log("Server listening on port %s", port)
